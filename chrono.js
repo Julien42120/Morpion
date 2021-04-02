d6f0284091312f3bb0f571295c99607532e41941
julien <julienmartin42120@gmail.com>
2021-04-02 10:09:42 +0200

    Morpion Final

--- chrono.js
+++ chrono.js
@@ -1,90 +1,41 @@
-var hours = 00;
-var minutes = 00;
-var seconds = 00;
-var milliseconds = 00;
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+let startButton = document.getElementById("start-btn");
+let allTime = document.getElementById("all-time");
 
-var h = document.querySelector("#hours");
-var m = document.querySelector("#minutes");
-var s = document.querySelector("#seconds");
-var ms = document.querySelector("#milliseconds");
-var start = document.querySelector("#startBtn");
-var stop = document.querySelector("#stopBtn");
-var reset = document.querySelector("#resetBtn");
-let interval;
+let secVal = 0;
+let minVal = 0;
+let counter = 0;
 
-function startTimer(){
-  
-  milliseconds++;
-//Add 0 before millisecond count under 10
-  if (milliseconds < 10){
-    ms.innerHTML = "0" + milliseconds;
-  }
-//Remove 0 above 10
-  if (milliseconds > 10){
-    ms.innerHTML = milliseconds;
-  }
-//Add a second, reinitiate millisecond count 
-  if (milliseconds > 99){
-    seconds++;
-    s.innerHTML = seconds;
-    milliseconds = 0;
-    ms.innerHTML = "0" + 0;
-  }
-//Add 0 before second count under 10
-  if (seconds < 10){
-    s.innerHTML = "0" + seconds;
-  }
-//Remove 0 above 10
-  if (seconds > 10){
-    s.innerHTML = seconds;
-  }
-//Add a minute, reinitiate second count   
-  if (seconds > 59){
-    minutes++;
-    m.innerHTML = minutes;
-    seconds = 0;
-    s.innerHTML = "0" + 0;
-  }
- //Add 0 before minute count under 10 
-  if (minutes < 10){
-    m.innerHTML = "0" + minutes;
-  }
- //Remove 0 above 10
-  if (minutes > 10){
-    m.innerHTML = minutes;
-  }
- //Add a hour, reinitiate minute count 
-  if (minutes > 59){
-    hours++;
-    h.innerHTML = hours;
-    minutes = 0;
-    m.innerHTML = "0" + 0;
-  }
- //Add 0 if hour under 10
-    if (hours < 10) {
-    h.innerHTML = "0" + hours;
-  }
+// Function used inside TimeHandler when 15 minutes is reached
+function myStopFunction() {
+    clearInterval(timer);
 }
 
-start.addEventListener('click', function(){
-  clearInterval(interval);
-  var oneSecond = 10;
-  interval = setInterval( startTimer, oneSecond);
-})
+// Main function
+function timeHandler() {
+    if (counter == 900) {
+        allTime.style.color = "red";
+        myStopFunction();
+        return;
+    }
+    // update counter each second
+    counter++;
+    secVal++;
+    sec.innerHTML = secVal;
 
-stop.addEventListener('click', function(){
-  clearInterval(interval);
-});
+    if (secVal == 60) {
+        sec.innerHTML = 0;
+        minVal++;
+        min.innerHTML = minVal;
+        secVal = 0;
+    }
+}
 
-reset.addEventListener('click', function(){
-  clearInterval(interval);
-  hours = 00;
-  minutes = 00;
-  seconds = 00;
-  milliseconds = 00;
-  h.innerHTML = "0" + hours;
-  m.innerHTML = "0" + minutes;
-  s.innerHTML = "0" + seconds;
-  ms.innerHTML = "0" + milliseconds;
-  
-})
+// Calls the function every second
+let timer = function() {
+    setInterval(timeHandler, 1000);
+}
+
+startButton.addEventListener("click", timer);
+startButton.click();
